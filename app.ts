06def3faa938e0e503271e265331a0e6727990df
Zhen Xu <zachar1xu@gmail.com>
2021-09-30 17:36:45 -0400

    customized logo and navbar icon complete

--- app.ts
+++ app.ts
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
+
 app.use('/', indexRouter);
 
 
