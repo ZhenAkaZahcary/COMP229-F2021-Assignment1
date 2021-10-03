71c861db1acb4158abde5eb379d8b4330b0d8f36
Zhen Xu <zachar1xu@gmail.com>
2021-10-03 16:46:14 -0400

    Contact page completed

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -5,4 +5,17 @@
     }
     window.addEventListener('load', Start);
 })();
+
+
+function BackToHome() 
+{
+    window.location.href = "/home";
+}
+
+try {
+    document.getElementById("button1").addEventListener("click", BackToHome, false  );
+} catch (error) {
+    
+}
+
 //# sourceMappingURL=app.js.map
